f988a3edad561f57e400af5a14ac07281129f672
Rakunn <benjamin.ignacio.castillo1897@gmail.com>
2026-06-28 04:13:23 -0400

    comitderellenoxd

--- Assets/Scripts/BossFight/Bosshealth.cs
+++ Assets/Scripts/BossFight/Bosshealth.cs
@@ -1,40 +1,66 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
+ 
 public class BossHealth : MonoBehaviour
 {
     [Header("HP")]
     public float maxHP = 100f;
-
+ 
+    [Header("Barra de Vida")]
+    public Image fillImage;
+ 
+    [Header("Texto de Vida")]
+    public TextMeshProUGUI lifeText;
+ 
     public float CurrentHP { get; private set; }
-
-    public event System.Action<float> OnHPChanged; // pasa el HP actual
+ 
+    public event System.Action<float> OnHPChanged;
     public event System.Action OnDeath;
-
+ 
     void Start()
     {
         CurrentHP = maxHP;
+        UpdateBar();
+        UpdateText();
     }
-
+ 
     public void TakeDamage(float amount)
     {
         if (CurrentHP <= 0f) return;
-
+ 
         CurrentHP = Mathf.Max(CurrentHP - amount, 0f);
         OnHPChanged?.Invoke(CurrentHP);
-
+        UpdateBar();
+        UpdateText();
+ 
         Debug.Log($"[Boss] HP: {CurrentHP}/{maxHP}");
-
+ 
         if (CurrentHP <= 0f)
         {
             Debug.Log("[Boss] Muerto.");
             OnDeath?.Invoke();
         }
     }
-
+ 
     public void Heal(float amount)
     {
         if (CurrentHP <= 0f) return;
         CurrentHP = Mathf.Min(CurrentHP + amount, maxHP);
         OnHPChanged?.Invoke(CurrentHP);
+        UpdateBar();
+        UpdateText();
+    }
+ 
+    void UpdateBar()
+    {
+        if (fillImage != null)
+            fillImage.fillAmount = CurrentHP / maxHP;
+    }
+ 
+    void UpdateText()
+    {
+        if (lifeText != null)
+            lifeText.text = $"Actual Life: {CurrentHP}/{maxHP}";
     }
 }
